82746fbf9eac30fe8e5d2ea3fe87c05a16a76608
hanjsnote <enlun@naver.com>
2026-08-07 20:26:26 +0900

    feat: proxy external schedule API with Express

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
-
+const axios = require("axios")
 const app = express(); 
 
-app.get("/", (req, res) => {    // Define a route for the root URL ("/") that sends a response "Server Running"
-    res.send("Server Running");
-});
+app.get("/api/list", async (req, res) => { 
+    
+    const response = await axios.post("https://live.ecomm-data.com/api/schedule/list", 
+        { 
+            date: "260807" 
+        });
+    
+        res.json(
+            response.data
+        );
+    });
 
 app.listen(3000, () => {    // Start the server and listen on port 3000
     console.log("Server is running on port 3000");
